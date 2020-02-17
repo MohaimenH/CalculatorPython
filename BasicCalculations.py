c3a7a88c35b9bab82ff15765eeb76132b975be46
MohaimenH <hassan08@my.yorku.ca>
2020-02-16 21:01:37 -0500

    Added all operations, loops until user tells it to stop.

--- BasicCalculations.py
+++ BasicCalculations.py
@@ -1,13 +1,49 @@
 print("Calculator started!")
 print("Type ADD for addition and SUB for subtraction")
 
+done = ""
+stop = ""
+num = []
+i = 0
+math = 0
 
 func = input("Please enter what you what to do: ")
 
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+while stop != "STOP":
 
-if func == "ADD":
-    print(num1 + num2)
+    temp = input("Please input a number or type STOP: ")
+    if temp == "STOP":
+        break
+
+    num.append(int(temp))
+
+while (done != "NO") & (done != "N"):
+
+    if func == "ADD":
+        for x in num:
+            math += x
+        print("The addition is: ", math)
+
+    elif func == "SUB":
+        math = num[0]
+        print(math)
+        for x in range(1, len(num)):
+            print(num[x])
+            math = math - num[x]
+        print("The subtraction is: ", math)
+
+    elif func == "MUL":
+        math = num[0]
+        for x in range(1, len(num)):
+            math = math * num[x]
+        print("The subtraction is: ", math)
+
+    elif func == "DIV":
+        math = num[0]
+        for x in range(1, len(num)):
+            math = math / num[x]
+        print("The subtraction is: ", math)
+
+    done = input("Do you want to do to any other operation?: ")
 
 print("Thanks for using the calculator!")
